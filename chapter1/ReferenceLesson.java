582197d43c1bc56d1d7eab659a37e7bb33fccf0b
marcoyimyaem <marcoyimyaem@gmail.com>
2025-10-27 03:51:07 +0000

    Day 14

--- chapter1/ReferenceLesson.java
+++ chapter1/ReferenceLesson.java
@@ -2,8 +2,8 @@ package chapter1;
 
 public class ReferenceLesson {
     public static void main(String[] args) {
-        java.util.Date today = new java.util.Date();
-        String greeting = "How are you?"; 
-        System.out.println(args[1]);
-    }
+            java.util.Date today = new java.util.Date();
+            String greeting = "How are you?"; 
+            System.out.println(args[1]);
+            }
 }
