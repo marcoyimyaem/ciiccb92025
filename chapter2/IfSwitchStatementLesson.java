e5bcef8f96ca64eed8c280b410f2def579b66323
marcoyimyaem <marcoyimyaem@gmail.com>
2025-10-29 03:58:20 +0000

    Day 16

--- chapter2/IfSwitchStatementLesson.java
+++ chapter2/IfSwitchStatementLesson.java
@@ -1,6 +1,6 @@
 package chapter2;
 
-public class IfStatementLesson {
+public class IfSwitchStatementLesson {
     public static void main(String[] args) {
         int hourOfDay=22;
         if(hourOfDay<11)
